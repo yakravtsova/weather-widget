5da91df4924eb35a7306fd366f29abe54f5de359
Margarita Kravtsova <margaritaselez@yandex.ru>
2023-02-10 13:54:39 +0500

    build optimized

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.optimization.delete("splitChunks");
+  },
+  filenameHashing: false,
 });
