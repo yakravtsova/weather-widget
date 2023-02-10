3ae1ff16dc7ae8a968d9b89161012cbb98a6950a
Margarita Kravtsova <margaritaselez@yandex.ru>
2023-02-10 16:18:46 +0500

    icons changed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-//import "@fortawesome/fontawesome-free/css/all.css";
-//import "@fortawesome/fontawesome-free/js/all.js";
 import "document-register-element/build/document-register-element";
 
 Vue.config.productionTip = false;
